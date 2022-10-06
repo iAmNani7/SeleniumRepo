0208db5a6565a8d07ef7106a429afd52ac6fe9d5
Tessolve1 <Tessolve1@192.168.0.117>
2022-10-06 18:58:22 +0530

    011

--- Automation/src/com/automation/practice/UploadResumeToNaukariDotCom.java
+++ Automation/src/com/automation/practice/UploadResumeToNaukariDotCom.java
@@ -1,6 +1,7 @@
 package com.automation.practice;
 
 import java.io.File;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
@@ -13,17 +14,22 @@ public class UploadResumeToNaukariDotCom extends BaseClass{
 	@Test
 	public void uploadResume()
 	{
-			driver=new ChromeDriver();
-			driver.get("https://www.naukri.com/");
-			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-			driver.findElement(By.id("login_Layer")).click();
-			driver.findElement(By.xpath("(//input)[1]")).sendKeys("naveenkumar.podilli@gmail.com");
-			driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys("");
-			driver.findElement(By.xpath("//button[@type='submit']")).click();
-			driver.findElement(By.xpath("//div[@class='btn btn-block btn-large white-text']")).click();
-			File f=new File("./data/NaveenkumarPodili_UpdatedResume.pdf");
-			String path=f.getAbsolutePath();
-			driver.findElement(By.id("attachCV")).sendKeys(path);
-			driver.close();
+		Scanner s=new Scanner(System.in);
+		driver=new ChromeDriver();
+		System.out.println("Enter your email");
+		String mailid=s.nextLine();
+		System.out.println("Enter your password");
+		String pwd=s.nextLine();
+		driver.get("https://www.naukri.com");
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.findElement(By.id("login_Layer")).click();
+		driver.findElement(By.xpath("(//input)[1]")).sendKeys(mailid);
+		driver.findElement(By.xpath("//input[@placeholder='Enter your password']")).sendKeys(pwd);
+		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//div[@class='btn btn-block btn-large white-text']")).click();
+		File f=new File("./data/NaveenkumarPodili_UpdatedResume.pdf");
+		String path=f.getAbsolutePath();
+		driver.findElement(By.id("attachCV")).sendKeys(path);
+		driver.close();
 	}
 }
